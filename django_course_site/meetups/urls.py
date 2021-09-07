7917c8c402a7d174a38f9004b723e267838d9cad
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-07 19:14:49 -0300

    dynamic url

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index),  # domain/meetup
-    path('meetups/<slug:meetup_slug>/', views.meetup_details)  # domain/meetups/<dynamic-path-segment>
+    path('meetups/', views.index, name='all-meetups'),  # domain/meetup
+    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail')
+    # domain/meetups/<dynamic-path-segment>
 ]
