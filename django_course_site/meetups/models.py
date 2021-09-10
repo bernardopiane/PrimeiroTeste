85a38abd3884795bb59b557a50d749025723753f
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-09 23:55:49 -0300

    Database query

--- django_course_site/meetups/models.py
+++ django_course_site/meetups/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+
+# precisa importar Model
+class Meetup(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
