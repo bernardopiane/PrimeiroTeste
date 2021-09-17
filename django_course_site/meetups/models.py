038f66c35d14b9207526f5fd9ed96a934062fac2
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-16 22:22:34 -0300

    Added location to the model (1-to-many)

--- django_course_site/meetups/models.py
+++ django_course_site/meetups/models.py
@@ -3,12 +3,22 @@ from django.db import models
 
 # Create your models here.
 
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+    address = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
 # precisa importar Model
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to="images")
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
 
     # pagina admin mostra como nome a string do modelo
     def __str__(self):
