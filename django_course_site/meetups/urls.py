9cacd113dc752a87b741d69ce2d48c42c469238e
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-20 20:12:32 -0300

    Registration success added

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'),
+    path('meetups/success', views.confirm_registration, name='confirm-registration'),
     # domain/meetup
-    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail')
+    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),
     # domain/meetups/<dynamic-path-segment>
 ]
