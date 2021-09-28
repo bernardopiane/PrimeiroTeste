d3ccfa09e454619e71afc2432d4192e06d737f09
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-28 20:57:49 -0300

    Organizer email into success page

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('meetups/', views.index, name='all-meetups'),
-    path('meetups/success', views.confirm_registration, name='confirm-registration'),
+    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
     # domain/meetup
     path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),
     # domain/meetups/<dynamic-path-segment>
