c9157e2b2f0ec3444685abf1a661fc4e9f327e3d
bernardopiane <bernardopiane@users.noreply.github.com>
2021-09-28 21:01:37 -0300

    root url redirects to /meetups

--- django_course_site/meetups/urls.py
+++ django_course_site/meetups/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index, name='all-meetups'),
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
+    path('', views.index, name='all-meetups'),
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),
     # domain/meetup
-    path('meetups/<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),
+    path('<slug:meetup_slug>/', views.meetup_details, name='meetup-detail'),
     # domain/meetups/<dynamic-path-segment>
 ]
